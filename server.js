2ba0918dc64fb4a644a4a01f8bd409cfc2fd37c7
Vedant <singhvedant1701@gmail.com>
2021-12-28 19:02:35 +0530

    problem of port allocation solved

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Defining port number
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // Function to serve all static files
 // inside public directory.
@@ -12,6 +12,7 @@ app.use(express.static('public'));
 app.use('/images', express.static('images'));
 
 // Server setup
+
 app.listen(PORT, () => {
     console.log(`Running server on PORT ${PORT}...`);
 })
